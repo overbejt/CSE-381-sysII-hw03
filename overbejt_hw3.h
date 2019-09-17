555cc93dad6738f1fb3cf0cd1e5563078f07b895
overbejt <overbejt@miamioh.edu>
2019-09-17 14:40:27 -0400

    Gotta go to bio

--- overbejt_hw3.h
+++ overbejt_hw3.h
@@ -1,7 +1,7 @@
 /* 
  * File:   overbejt_hw3.h
  * Author: Josh Overbeck
- * Description: Thi is the header file for homework assignment 3 in CSE 381.
+ * Description: This is the header file for homework assignment 3 in CSE 381.
  *
  * Created on September 17, 2019, 2:17 PM
  * copyright (c) 2019 overbejt@miamioh.edu
@@ -9,7 +9,21 @@
 
 #ifndef OVERBEJT_HW3_H
 #define OVERBEJT_HW3_H
-class overbejt_hw3{  
+
+#include <unordered_map>
+#include <string>
+
+class overbejt_hw3{
+private:
+    std::unordered_map<int, int> pid_pid;
+    std::unordered_map<int, std::string> pid_cmd;
+public:
+    overbejt_hw3()
+    {
+    
+    }
+    void loadData();
+    void printTree();
 };
 #endif /* OVERBEJT_HW3_H */
 
